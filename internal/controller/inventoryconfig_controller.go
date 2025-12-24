a8808df5f9b64f94298954c53ac290c58edc23ac
srimandarbha <sriman1988@gmail.com>
2025-12-25 00:00:31 +0530

    Add log statement for Reconcile trigger

--- internal/controller/inventoryconfig_controller.go
+++ internal/controller/inventoryconfig_controller.go
@@ -25,7 +25,7 @@ type InventoryReconciler struct {
 
 func (r *InventoryReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	l := log.FromContext(ctx)
-
+    fmt.Printf(">>> Reconcile triggered for: %s in namespace %s\n", req.Name, req.Namespace)
 	// Fetch the VM from the local cache
 	var vm kubevirtv1.VirtualMachine
 	if err := r.Get(ctx, req.NamespacedName, &vm); err != nil {
